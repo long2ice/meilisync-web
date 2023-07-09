61d357fcfffb7defa4da40b6c14351af1b34135a
long2ice <long2ice@gmail.com>
2023-07-09 21:33:47 +0800

    refactor: remove license

--- src/api/license.ts
+++
@@ -1,7 +0,0 @@
-import http from '@/axios'
-import type { LicenseResponse } from '@/types/responses'
-
-export async function getLicense(): Promise<LicenseResponse> {
-  const { data } = await http.get('/license')
-  return data
-}
